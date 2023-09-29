25854ea174c455522c04b6def0f50f2eadee9050
jjackk8 <whitefordjack03@gmail.com>
2023-09-29 09:39:27 +0100

    links and adds a new JS thing

---
+++ main.js
@@ -0,0 +1,24 @@
+//if ( s.length > 10 ) {
+  //s=s.substring(0, 10);
+//} else {
+//  s=s.trim();
+//}
+
+//switch ( Math.floor(Math.random()*3) ) {
+//  case 1: col = "red";
+//  break;
+//  case 2: col = "blue";
+//  break;
+//  default: col = "green";
+//}
+
+console.log("hello world");
+
+let d = document.createElement('div')
+
+
+d.style.fontSize = "20px";
+d.style.width = "80%";
+
+console.log(d);
+document.body.appendChild(d);
