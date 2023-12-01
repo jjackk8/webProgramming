0c7af1c307b4caf7cc231947b725949c9698923b
jjackk8 <whitefordjack03@gmail.com>
2023-12-01 13:44:52 +0000

    attempt at event listener on image

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-document.querySelector("html").addEventListener("click", function () {
-  alert("Thank you for clicking!");
+
+document.getElementById("dog").src.addEventListener("click", () => {
+  alert("A dog may be a good fit for you!");
 });
 
-
+document.getElementById("cat").src.addEventListener("click", () => {
+  alert("A cat may be a good fit for you!");
+});
 
